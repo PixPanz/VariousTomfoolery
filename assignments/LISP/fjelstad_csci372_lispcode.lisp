18f3e4c9a3eb196741445829047a7c136687bce2
Spencer Fjelstad <fjelstad1678@gmail.com>
2019-12-23 02:04:03 -0600

    Just some slight changes to make output look nicer and added a file containing the actual program output

--- assignments/LISP/fjelstad_csci372_lispcode.lisp
+++ assignments/LISP/fjelstad_csci372_lispcode.lisp
@@ -1,7 +1,9 @@
 ;LISP Assignment - CSCI 372
 ;Spencer Fjelstad
 ;Problem 1
+(format t"Testing lambda function:~%")
 (format t"(lambda)'(1 2 3 4) returns ~S~%" (mapcar (lambda (lis)(* lis 2)) '(1 2 3 4)))
+(format t"(lambda)'(1 3 5 7) returns ~S~%" (mapcar (lambda (lis)(* lis 2)) '(1 3 5 7)))
 
 ;Problem 2
 (defun MAKE-SET (lis)
@@ -59,16 +61,30 @@
 	)
 )
 
+(format t"~%Testing MAKE-SET function:~%")
 (format t"(MAKE-SET '(1 2 3 2 3 1 4)) returns ~S~%"(MAKE-SET '(1 2 3 2 3 1 4)))
+(format t"(MAKE-SET '(1 2 3 4 5 6)) returns ~S~%"(MAKE-SET '(1 2 3 4 5 6)))
+
+(format t"~%Testing IS-SET function:~%")
 (format t"(IS-SET '(1 2 3 2 3 1 4)) returns ~S~%"(IS-SET '(1 2 3 2 3 1 4)))
 (format t"(IS-SET '(1 2 3 4)) returns ~S~%"(IS-SET '(1 2 3 4)))
+
+(format t"~%Testing MY-LEN function:~%")
 (format t"(MY-LEN '(1 2 3 4)) returns ~S~%"(MY-LEN '(1 2 3 4)))
 (format t"(MY-LEN '()) returns ~S~%"(MY-LEN '()))
+
+(format t"~%Testing MY-SUM function:~%")
 (format t"(MY-SUM '(1 2 3 4 5)) returns ~S~%"(MY-SUM '(1 2 3 4 5)))
 (format t"(MY-SUM '()) returns ~S~%"(MY-SUM '()))
+
+(format t"~%Testing MY-AVERAGE function:~%")
 (format t"(MY-AVERAGE '(1 2 3 4 5)) returns ~S~%"(MY-AVERAGE '(1 2 3 4 5)))
 (format t"(MY-AVERAGE '(1 2 1 2)) returns ~S~%"(MY-AVERAGE '(1 2 1 2)))
+
+(format t"~%Testing MAX-SUB-LIST function:~%")
 (format t"(MAX-SUB-LIST '((1) (1 2) (1 2 3))) returns ~S~%"(MAX-SUB-LIST '((1) (1 2) (1 2 3))))
 (format t"(MAX-SUB-LIST '((1 2 3) (1 2) (1))) returns ~S~%"(MAX-SUB-LIST '((1 2 3) (1 2) (1))))
+
+(format t"~%Testing MYMEMBER function:~%")
 (format t"(MYMEMBER 3 '(1 2 3 4)) returns ~S~%"(MYMEMBER 3 '(1 2 3 4)))
 (format t"(MYMEMBER 5 '(1 2 3 4)) returns ~S~%"(MYMEMBER 5 '(1 2 3 4)))
